93c748689063249018eeeab5f7dbba8c2e9222a6
laisRamos1234 <laisramos123@gmail.com>
2023-05-08 19:00:28 -0300

    Inicio do projeto

---
+++ src/main/java/com/example/biograph2/BioGraph2Application.java
@@ -0,0 +1,13 @@
+package com.example.biograph2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioGraph2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BioGraph2Application.class, args);
+    }
+
+}
